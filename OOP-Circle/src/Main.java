a2564473041528bff521909ec5f93ecb155b0ff8
Bozhidar Stamenov <bjstamenov@gmail.com>
2023-07-03 16:54:04 +0300

    OOP-exercises

---
+++ OOP-Circle/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(5.6);
+
+        System.out.println("The area of the circle is: " + circle.getArea());
+        System.out.println("The circumference  of the circle is: " + circle.getCircumference());
+    }
+}
